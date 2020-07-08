983be42826492f1fab286c8bfd384588ffdc9f47
[Brandon Teague] <[teaguebrandon@outlook.com]>
2020-07-08 18:26:37 -0400

    finished mvp for day 1 and 2 and working on stretch

--- src/item.py
+++ src/item.py
@@ -1,7 +1,6 @@
 # Write a class to hold player information, e.g. what room they are in
 # currently.
-class Player:
-    def __init__(self,name,room):
+class Item:
+    def __init__(self, name, lightsource):
         self.name = name
-        self.room = room
-        self.inventory = []
+        self.lightsource = lightsource
--- src/room.py
+++ src/room.py
@@ -1,2 +1,15 @@
 # Implement a class to hold room information. This should have name and
 # description attributes.
+class Rooms:
+    def __init__(self, name, description, items, is_lit, n_to=None, s_to=None, e_to=None, w_to=None):
+        self.name = name
+        self.description = description
+        self.n_to = n_to
+        self.s_to = s_to
+        self.e_to = e_to
+        self.w_to = w_to
+        self.is_lit = is_lit
+        self.items = items
+
+    def __str__(self):
+        return f'Current location: {self.name}. Description: {self.description} Items in room: {self.items}'
